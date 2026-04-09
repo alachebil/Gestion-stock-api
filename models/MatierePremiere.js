5998ee109e8244133dc38b7984b5d4c67d5c9a44
alachebil <alaa.chebil2000@gmail.com>
2026-04-09 22:36:12 +0100

    Correction des types d'énumération pour les modèles MatierePremiere et ProduitSemiPret

--- models/MatierePremiere.js
+++ models/MatierePremiere.js
@@ -5,7 +5,7 @@ const matierePremiereSchema = new mongoose.Schema(
     nom: { type: String, required: true },
     type: {
       type: String,
-      enum: ["Type 1", "Type 2", "Type 3"],
+      enum: ["base", "bargatere"],
       required: true,
     },
     quantiteKg: { type: Number, required: true, default: 0 },
